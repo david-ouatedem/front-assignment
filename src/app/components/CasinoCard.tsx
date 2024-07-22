f75261cc2206c8904f78eba81330da6d60eae433
david-ouatedem <davdev1400@gmail.com>
2024-07-22 20:01:53 +0100

    fixed responsive

--- src/app/components/CasinoCard.tsx
+++ src/app/components/CasinoCard.tsx
@@ -22,8 +22,8 @@ const CasinoCard: React.FC<OwnProps> = ({
   className,
 }) => {
   return (
-    <div className="rounded bg-white customDropShadow p-4 w-full lg:flex items-center">
-      <div className="md:grid grid-cols-2 md:gap-4 lg:flex lg:shrink-0 lg:gap-4 w-full lg:w-fit">
+    <div className="rounded bg-white customDropShadow p-4 w-full lg:flex items-center lg:gap-5">
+      <div className="md:grid grid-cols-2 md:gap-4 lg:flex lg:gap-4 xl:gap-12 w-full">
         <div
           className={twMerge(
             "flex items-center justify-center w-full h-[132px] md:h-full lg:max-w-[224px] lg:shrink-0",
@@ -40,7 +40,7 @@ const CasinoCard: React.FC<OwnProps> = ({
             />
           </figure>
         </div>
-        <div className="lg:flex lg:gap-12 lg:items-center">
+        <div className="lg:flex lg:gap-12 xl:gap-20 lg:items-center shrink-0">
           <div>
             <a href="#">
               <h2
@@ -60,20 +60,20 @@ const CasinoCard: React.FC<OwnProps> = ({
               ))}
             </div>
           </div>
-          <div className="mt-5 md:mt-8 lg:mt-0 flex flex-col items-center md:items-start lg:w-full">
+          <div className="mt-5 md:mt-8 lg:mt-0 flex flex-col items-center md:items-start lg:items-center lg:w-full">
             <p className="font-medium !font-[600] text-xl text-black leading-[24px]">
               Bonus
             </p>
-            <p className="font-regular text-[28px] text-black text-center md:text-start leading-[34px]">
+            <p className="font-regular text-[28px] text-black text-center md:text-start lg:text-center leading-[34px]">
               Bet £10 <br className="md:hidden lg:block" /> Get £400
             </p>
           </div>
         </div>
       </div>
-      <div className="w-full max-w-[190px]">
+      <div className="w-full flex justify-end xl:justify-center">
         <button
           type="button"
-          className="mt-4 md:mt-5 lg:mt-0 font-[700] font-regular text-xl rounded-[10px] h-[64px] w-full bg-secondary text-white text-center hover:bg-[#056A21] transition-all duration-300 ease-out"
+          className="lg:max-w-[190px] xl:max-w-[308px]  mt-4 md:mt-5 lg:mt-0 font-[700] font-regular text-xl rounded-[10px] h-[64px] w-full bg-secondary text-white text-center hover:bg-[#056A21] transition-all duration-300 ease-out"
         >
           Bet Now
         </button>
